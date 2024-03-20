a407dcc472814335092d086bc9e14eb3ff4982a6
tuesdaychicken <vitaobaek@kakao.com>
2024-03-21 01:55:59 +0900

    코테 2일차 마지막 문제 풀이 너무 오래 걸렸다 반성하자

--- Programmers_DAY2_010.java
+++ Programmers_DAY2_010.java
@@ -1,6 +1,7 @@
 package CodingTest_Troubleshooting;
 
-
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 /*
 문제설명
@@ -33,45 +34,83 @@ package CodingTest_Troubleshooting;
 public class Programmers_DAY2_010 {
 
     public static void main(String[] args) {
-
-        solution("He11oWor1d", "lloWorl", 2);
+        //TEST를 위해 추가한 main
+        //solution("asdfqszcxqaw", "QQQQ",11); 조건을 잘 보자 규성아 이 개
         
     }
 
     public static String solution(String my_string, String overwrite_string, int s) {
-        String answer = "";
-        int num = 0;
-
-        String [] answer1 = my_string.split("");
-        String [] overwite1 = overwrite_string.split("");
+        String[] a = my_string.split("");
+        String[] b = overwrite_string.split("");
         
-        for(int i = 0; i < answer1.length; i++){
-            if(i >= s-1 && i < s - 1 + overwrite_string.length()){
-                answer1[i+1] = overwite1[num];
-                num++;
-            }
+        for (int i = s; i < b.length + s; i++) {
+            a[i] = b[i - s];
         }
-        answer = String.join("", answer1);
-        // 내가 입력한 값에 s번째니까 s가 2라면 i가 1일 때 해당 됨 
-        //    그리고 들어오는 over의 길이만큼만 실행해야함
-        // He11oWor1d lloWorl 2
-        // 
-        //  
-        // 
-        // HelloWorld
-       
-        System.out.println(answer);
-        return answer;
+        
+        return Arrays.stream(a).collect(Collectors.joining());
     }
 }
-// answer = my_string.substring(0,s) + overwrite_string;
+
 // 접근 1. 문자열을 입력하고 입력한 정수만큼의 뒷 부분을 날린 후 이어 붙인다
 // 접근 2. 문자열을 배열에 넣고 해당 위치부터 새로운 문자를 넣고 출력
 
 /*      
     다른 사람 풀이(1)
-        
+        String before = my_string.substring(0, s);
+        String after = my_string.substring(s + overwrite_string.length());
+        return before + overwrite_string + after;
 
     다른 사람 풀이(2)
+        String answer = my_string.substring(0, s) + overwrite_string;
+
+        if(my_string.length() > answer.length()) {
+            answer += my_string.substring(answer.length());
+        }
+        return answer;
+    
+    다른 사람 풀이(3)
+        char[] my_chars = my_string.toCharArray();
+        char[] overwrite_chars = overwrite_string.toCharArray();
+        for (int i=0; i<overwrite_chars.length; i++) {
+            my_chars[s + i] = overwrite_chars[i];
+        }
+        return String.valueOf(my_chars);
+*/
+
+/*  나의 잘못된 접근
+    실수 1.
+그 다음을 어떻게 접근해야하는지 생각을 못했다
+        answer = my_string.substring(0,s) + overwrite_string;
+
+    실수 2.
+코드를 짜다보니 스파게티 코드 및 에일리언 코드가 되어버렸다
+이것은 엄청난 거다 에일리언 스파게티... 변수명을 잘 짓자
+        String answer = "";
+        int num = 0;
+
+        String [] answer1 = my_string.split("");
+        String [] overwite1 = overwrite_string.split("");
+
+        if(my_string.length() < s + overwrite_string.length()){
+            
+            List<String> list = new ArrayList<>(Arrays.asList(answer1));
+            for(int i=0; i<s+overwrite_string.length()-my_string.length(); i++){
+                list.add(" ");
+            }
+            answer1 = list.toArray(new String[0]);
+
+        }
+        
+        for(int i = 0; i < s + overwrite_string.length(); i++){
+            if(i >= s-1 && i < s - 1 + overwrite_string.length()){
+                answer1[i+1] = overwite1[num];
+                num++;
+                System.out.println(Arrays.toString(answer1));
+            }
+        }
+        answer = String.join("", answer1);
+       
+        System.out.println(answer);
+        return answer;
         
 */
