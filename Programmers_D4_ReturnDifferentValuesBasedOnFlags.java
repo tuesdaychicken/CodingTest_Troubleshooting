c077a881627868986b7752ee0fff8ee706e1c1f1
tuesdaychicken <vitaobaek@kakao.com>
2024-03-24 22:53:46 +0900

    D4 문제만 간단하게 등록

---
+++ Programmers_D4_ReturnDifferentValuesBasedOnFlags.java
@@ -0,0 +1,47 @@
+package CodingTest_Troubleshooting;
+
+/*
+문제설명
+    두 정수 a, b와 boolean 변수 flag가 매개변수로 주어질 때, 
+    flag가 true면 a + b를 false면 a - b를 return 하는 
+    solution 함수를 작성해 주세요.
+   
+제한사항
+    -1,000 ≤ a, b ≤ 1,000
+   
+입출력 예
+     a	 b	 flag	 result
+    -4	 7	 true	 3
+    -4	 7	 false	 -11
+
+입출력 예
+
+입출력 예 설명
+    입출력 예 #1
+        예제 1번에서 flag가 true이므로 a + b = (-4) + 7 = 3을 return 합니다.
+
+    입출력 예 #2
+        예제 2번에서 flag가 false이므로 a - b = (-4) - 7 = -11을 return 합니다.
+ */
+
+public class Programmers_D4_ReturnDifferentValuesBasedOnFlags {
+    public int solution(int a, int b, boolean flag) {
+        int answer = 0;
+        return answer;
+    }
+}
+
+// 접근 1. 
+
+/*      
+    다른 사람 풀이(1)
+        
+
+    다른 사람 풀이(2)
+        
+*/
+
+/*  나의 잘못된 접근
+    실수 1.
+        
+*/
